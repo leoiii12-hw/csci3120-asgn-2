b31bd2518af0882713d6d5f6cb782ce63e907769
Leo Choi <choimankin@gmail.com>
2018-03-07 00:36:00 +0800

    1. Done error recovery 3

--- Main.java
+++ Main.java
@@ -6,8 +6,9 @@ public class Main {
     public static void main(String[] args) {
         try {
             Program root = new MiniJavaParser(System.in).Goal();
-            root.accept(new Task3Visitor());
+            root.accept(new Task2Visitor());
         } catch (ParseException e) {
+            e.printStackTrace();
             System.out.println(e.toString());
         }
     }
--- input/Factorial.java
+++ input/Factorial.java
@@ -1,28 +1,48 @@
-class Factorial{
-    public static void main(String[] a){
-	System.out.println(new Fac().ComputeFac(10));
-    }
+class Factorial {
+  public static void main(String[] a) {
+    System.out.println(new Fac().ComputeFac(10));
+  }
 }
 
 class Fac {
 
-    public int ComputeFac((int num, y, x, char x,y int x){
-	int num_aux ;
+    public int ComputeFac(int num, y, x, char x, y int x) {
+    int num_aux;
 
-	if (num < 1)
-	    num_aux = 1 ;
-	else
-	    num_aux = num * (this.ComputeFac(num-1)) ;
+    if (num < 1)
+      num_aux = 1;
+    else
+      num_aux = num * (this.ComputeFac(num - 1));
 
-	for(int i = 2, b=3,c=   4; !(i < 10); i=i^^2) {
-			System.out.println(i);
-			System.out.println(i);
-			System.out.println(i);
-			System.out.println(i);
-		}
-
-	return num_aux ;
+    for (int i = 2, b = 3, c = 4; !(i < 10); i = i ^^2){
+      System.out.println(i);
+      System.out.println(i);
+      System.out.println(i);
+      System.out.println(i);
     }
+
+    return num_aux;
+  }
+
+      public int c1(int x        (, int y, (*j) {
+
+    return 1;
+  }
+
+      public int c2(int x, int k,     (*j) {
+
+    return 1;
+     }
+
+      public int c3((int x, int y, int j) {
+
+    return 1;
+   }
+
+      public int c4(int   x, int y,         ++j) {
+
+    return 1;
+  }
 
 
 }
